6e728d4872ba69a620930a38362129a9a6d25d99
hzxo4o9 <34123003+hzxo4o9@users.noreply.github.com>
2018-09-03 10:21:13 +0800

    回文数
    判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。

---
+++ isPalindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if (x<0):
+            return False
+        if(x//10==0):
+            return True
+        if (x>0):
+            x = list(str(x))
+            x=[int(i) for i in x]
+            for i in range(0,len(x)//2):
+                if(x[i] != x[len(x)-1-i]):
+                    return False
+                
+            return True
+        
+            
